a729ddaf468e0a9c6f5d16f45e9aee994bc5c7c2
pguyon <pguyon69@gmail.com>
2022-08-27 13:33:55 +0200

    add underline on active link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import logo from '../../assets/logo-kasa.png'
 import './Header.css'
 
 
 const Header = () => {
+
+    const activeLink = useLocation();
+
     return (
         <div className='header-wrapper'>
             <img className='header__img' src={logo}
                 alt=''></img>
-            <li>
-                <Link className='header__link' to="/">Accueil</Link>
-                <Link className="header__link" to="/about">A propos</Link>
-            </li>
+            <nav>
+                <Link className='header__link' to="/">
+                <li className={activeLink.pathname === '/' ? "header__link__active active" : "header__link"}>Accueil</li>
+                </Link>
+                <Link className="header__link" to="/about">
+                <li className={activeLink.pathname === '/about' ? "header__link__active active" : "header__link"}>A propos</li>
+                </Link>
+            </nav>
         </div>
     );
 }
