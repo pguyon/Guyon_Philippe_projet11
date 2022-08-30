63649e80a979db1f6c7a7e5aede148faedafbe13
pguyon <pguyon69@gmail.com>
2022-08-30 13:07:47 +0200

    add Dropdown.jsx

--- src/UI/dropdown/Dropdown.jsx
+++ src/UI/dropdown/Dropdown.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './Dropdown.css'
+import './Dropdown.css';
+import ArrowDown from '../arrow/ArrowDown';
+import ArrowUp from '../arrow/ArrowUp';
 
 const Dropdown = ({title, description }) => {
     const [show, setShow] = useState(false);
+    
 
     const toggle = () => {
         setShow(!show)
@@ -13,7 +16,7 @@ const Dropdown = ({title, description }) => {
         <div>
             <div className='dropdown__title'>
                 <h2>{title}</h2>
-             <button onClick={toggle}>+</button>
+             <button onClick={toggle} >{show ?  <ArrowUp/>: <ArrowDown/>}</button>
             </div>
             {show ? <div className='dropdown__description'><p>{description}</p></div> : ''}
         </div>
