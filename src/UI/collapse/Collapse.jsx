d92965a34b31771c96c9c3ce1e985e6ca494fbc6
pguyon <pguyon69@gmail.com>
2022-08-30 13:24:51 +0200

    add Dropdown.jsx

--- src/UI/collapse/Collapse.jsx
+++ src/UI/collapse/Collapse.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './Dropdown.css';
+import './Collapse.css';
 import ArrowDown from '../arrow/ArrowDown';
 import ArrowUp from '../arrow/ArrowUp';
 
-const Dropdown = ({title, description }) => {
+const Collapse = ({title, description }) => {
     const [show, setShow] = useState(false);
     
 
@@ -14,18 +14,18 @@ const Dropdown = ({title, description }) => {
 
     return (
         <div>
-            <div className='dropdown__title'>
+            <div className='collapse__title'>
                 <h2>{title}</h2>
              <button onClick={toggle} >{show ?  <ArrowUp/>: <ArrowDown/>}</button>
             </div>
-            {show ? <div className='dropdown__description'><p>{description}</p></div> : ''}
+            {show ? <div className='collapse__description'><p>{description}</p></div> : ''}
         </div>
     );
 }
 
-Dropdown.propTypes = {
+Collapse.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string
 }
 
-export default Dropdown;
+export default Collapse;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './About.css';
 import Banner from '../../UI/banner/Banner';
 import AboutData from '../../data/AboutData.js'
-import Dropdown from '../../UI/dropdown/Dropdown';
+import Collapse from '../../UI/collapse/Collapse';
 
 const About = () => {
     
@@ -13,7 +13,7 @@ console.log(AboutData);
         <main>
             <Banner />
             {AboutData.map((item, index) => (
-        <Dropdown key={item[0] + index} title={item[0]} description={item[1]} />
+        <Collapse key={item[0] + index} title={item[0]} description={item[1]} />
       ))}
         </main>
     );
