d75a0bc159b0fa52712b9f853ebf13afa9820ced
pguyon <pguyon69@gmail.com>
2022-08-31 09:32:50 +0200

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Footer.css';
+import Logo from '../../assets/logo-kasa-white.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className='footer__wrapper'>
+            <div className='footer__content'>
+                <img  className='footer__img' src={Logo} alt=''/>
+                <p>@ Kasa. All rights reserved</p>
+            </div>
+        </footer>
     );
 }
 
