bb8fb5817cffbaad90697f65560fabdee9256cf4
pguyon <pguyon69@gmail.com>
2022-09-01 11:18:36 +0200

    add useLocation on Collapse.jsx

--- src/UI/collapse/Collapse.jsx
+++ src/UI/collapse/Collapse.jsx
@@ -1,31 +1,40 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Collapse.css';
-import ArrowDown from '../arrow/ArrowDown';
-import ArrowUp from '../arrow/ArrowUp';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Collapse.css";
+import ArrowDown from "../arrow/ArrowDown";
+import ArrowUp from "../arrow/ArrowUp";
 
-const Collapse = ({title, description }) => {
-    const [show, setShow] = useState(false);
-    
+const Collapse = ({ title, description }) => {
+  const [show, setShow] = useState(false);
+  const location = useLocation();
 
-    const toggle = () => {
-        setShow(!show)
-    }
+  const toggle = () => {
+    setShow(!show);
+  };
 
+  if (location.pathname === "/about") {
     return (
-        <div>
-            <div className='collapse__title'>
-                <h2>{title}</h2>
-             <button onClick={toggle} >{show ?  <ArrowUp/>: <ArrowDown/>}</button>
-            </div>
-            {show ? <div className='collapse__description'><p>{description}</p></div> : ''}
+      <div>
+        <div className="collapse__title">
+          <h2>{title}</h2>
+          <button onClick={toggle}>{show ? <ArrowUp /> : <ArrowDown />}</button>
         </div>
+        {show ? (
+          <div className="collapse__description">
+            <p>{description}</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     );
-}
+  }
+};
 
 Collapse.propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string
-}
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
 
 export default Collapse;
