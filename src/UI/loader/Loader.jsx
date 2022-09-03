9888b96efb930a371a039c19fe385e7d93c83342
pguyon <pguyon69@gmail.com>
2022-09-03 09:25:50 +0200

    add loader

---
+++ src/UI/loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Loader.css'
+
+const Loader = () => {
+    return (
+        <div className="loading">
+        <div className="loading">
+            <div className="loading__one">
+                <div className="loading__two"></div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Loader;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react';
 import Banner from '../../UI/banner/Banner';
 import Card from '../../UI/card/Card';
 import {Link} from 'react-router-dom';
+import Loader from '../../UI/loader/Loader';
 import './Home.css'
 
 const Home = () => {
     const [logement, setLogement] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         fetch('logements.json', {
@@ -16,39 +18,46 @@ const Home = () => {
         }).then((res) => {
             return res = res.json()
         }).then((data) => {
-            setLogement(data)
+            setLogement(data);
+            setIsLoading(true)
         })
 
     }, [])
     
-    return (
-        <main>
-            <Banner/>
-            <section className='section__wrapper'> {
-                logement.map((item) => (
-                    <Link className='section__link' key={
-                            item.id
-                        }
-                        to={
-                            `/logement/${
+    if(!isLoading){
+        return <Loader />
+    } else {
+        return (
+            <main>
+                <Banner/>
+                <section className='section__wrapper'> {
+                    logement.map((item) => (
+                        <Link className='section__link' key={
                                 item.id
-                            }`
-                    }>
-                        <Card picture={
-                                item.cover
                             }
-                            pictureAlt={
-                                item.title
-                            }
-                            title={
-                                item.title
-                            }/>
-                    </Link>
-                ))
-            } 
-            </section>
-        </main>
-    );
+                            to={
+                                `/logement/${
+                                    item.id
+                                }`
+                        }>
+                            <Card picture={
+                                    item.cover
+                                }
+                                pictureAlt={
+                                    item.title
+                                }
+                                title={
+                                    item.title
+                                }/>
+                        </Link>
+                    ))
+                } 
+                </section>
+            </main>
+        );
+    }
+
+   
 }
 
 export default Home;
