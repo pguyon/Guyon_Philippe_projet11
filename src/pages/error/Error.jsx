04b31a933fce1ab0236af2fde8a09e877f22f6fd
pguyon <pguyon69@gmail.com>
2022-09-03 09:13:37 +0200

    add css on Error.jsx

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Error.css'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
-        <div>
-            <h1>Error</h1>
-        </div>
+        <main className='error__wrapper'>
+            <h1 className='error__title'>404</h1>
+            <p className='error__para'>Oups! La page que vous demandez n'éxiste pas</p>
+            <Link className='error__link' to="/">Retourner à la page d'accueil</Link>
+        </main>
     );
 }
 
